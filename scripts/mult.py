20ebf8fedf829d7295225efa40dbb410a7194678
RyotaHama07 <92443822+RyotaHama07@users.noreply.github.com>
2022-01-07 15:06:33 +0900

    Update mult.py

--- scripts/mult.py
+++ scripts/mult.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
+
+# BSD-3-Clause License
+# Copyright (c) 2021 Ryuichi Ueda and Hama Ryota All rights reserved.
+
 import rospy
 from std_msgs.msg import Int32
 import random
